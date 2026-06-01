01dd644a7d7bec0efc8181d160912812c3938d37
Amaldev M <amaldevm.dev@gmail.com>
2026-06-01 15:52:23 +0530

    Integrate global Framer Motion page slide-and-fade routing transitions inside UserLayout.jsx

--- client/src/layout/UserLayout.jsx
+++ client/src/layout/UserLayout.jsx
@@ -4,6 +4,7 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/user/Footer";
 import { UserHeader } from "@/components/user/UserHeader";
 import AIChatbot from "../components/user/AIChatbot";
+import { motion, AnimatePresence } from "framer-motion";
 
 
 export function UserLayout() {
@@ -18,8 +19,18 @@ export function UserLayout() {
   return (
     <div>
       {isUserAuth ? <UserHeader /> : <Header />}
-      <div className='min-h-96'> 
-        <Outlet />
+      <div className='min-h-96 overflow-hidden'> 
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -15 }}
+            transition={{ duration: 0.28, ease: "easeInOut" }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </div>
       <AIChatbot />
       <Footer />
