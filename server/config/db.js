8e91db7b46dd7607f1e5e8f4fa9396ab618b9cbc
Amaldev M <amaldevm.dev@gmail.com>
2026-06-02 11:36:40 +0530

    Do not crash server when MongoDB connection fails

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1);
+    // Keep server running in development even if database connection fails
   }
 };
 
