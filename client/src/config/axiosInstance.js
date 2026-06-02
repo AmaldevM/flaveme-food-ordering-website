b40b1e91fbb91a7c8d1c4c5bbeb4c9920cb01759
Amaldev M <amaldevm.dev@gmail.com>
2026-06-02 14:19:27 +0530

    Fix user creation by adding default API fallback to client axiosInstance

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const apiBaseUrl = import.meta.env.VITE_API_URL && import.meta.env.VITE_API_URL !== "undefined"
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:4000";
+
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+    baseURL: `${apiBaseUrl}/api/v1`,
     withCredentials: true
 })
 
